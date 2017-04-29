a6d3cbb8d8886a05e81a17211dee123747fdc9ef
n_mogi <nobuyuki_mogi@rosso-tokyo.co.jp>
2017-04-29 09:05:37 +0900

    楽天API追加 一旦コミット

---
+++ jupiter/database/migrations/2017_04_28_093911_create_books_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('isbn_code', 13);
+            $table->string('title');
+            $table->string('author');
+            $table->string('publisher');
+            $table->string('genre');
+            $table->string('leading_status');
+            $table->string('borrower_user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
